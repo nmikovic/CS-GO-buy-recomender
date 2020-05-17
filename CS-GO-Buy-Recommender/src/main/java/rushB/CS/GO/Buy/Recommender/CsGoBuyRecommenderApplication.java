4781fa787e4d804dc24a8d80cb366cd155b4bce9
Dragan Ćulibrk <33953768+draganculibrk9@users.noreply.github.com>
2020-05-17 11:41:06 +0200

    Initial Spring Boot project

---
+++ CS-GO-Buy-Recommender/src/main/java/rushB/CS/GO/Buy/Recommender/CsGoBuyRecommenderApplication.java
@@ -0,0 +1,13 @@
+package rushB.CS.GO.Buy.Recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsGoBuyRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsGoBuyRecommenderApplication.class, args);
+	}
+
+}
---
+++ CS-GO-Buy-Recommender/src/test/java/rushB/CS/GO/Buy/Recommender/CsGoBuyRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package rushB.CS.GO.Buy.Recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsGoBuyRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
