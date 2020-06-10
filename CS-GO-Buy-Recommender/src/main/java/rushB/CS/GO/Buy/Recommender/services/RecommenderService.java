4715c4d9c037d24e7194a3ea762972d5a89984bb
Dragan Ćulibrk <33953768+draganculibrk9@users.noreply.github.com>
2020-06-10 12:36:40 +0200

    Bug in rules
    "Buy kevlar if not ECO or first round" rule buys kevlar for first player only

--- CS-GO-Buy-Recommender/src/main/java/rushB/CS/GO/Buy/Recommender/services/RecommenderService.java
+++ CS-GO-Buy-Recommender/src/main/java/rushB/CS/GO/Buy/Recommender/services/RecommenderService.java
@@ -2,41 +2,38 @@ package rushB.CS.GO.Buy.Recommender.services;
 
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.rule.QueryResults;
-import org.kie.api.runtime.rule.QueryResultsRow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rushB.CS.GO.Buy.Recommender.dtos.RoundInput;
-import rushB.CS.GO.Buy.Recommender.facts.*;
+import rushB.CS.GO.Buy.Recommender.facts.Armament;
+import rushB.CS.GO.Buy.Recommender.facts.Player;
+import rushB.CS.GO.Buy.Recommender.facts.PlayerStatus;
+import rushB.CS.GO.Buy.Recommender.facts.Round;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
 @Service
 public class RecommenderService {
-
     @Autowired
     private KieContainer kieContainer;
 
     @Autowired
     private HashMap<String, Armament> armaments;
     private KieSession session;
-    private Map map;
-    private ArrayList<Player> players;
     private Integer roundCounter = 1;
 
     private void prepareSession(RoundInput roundInput) {
-        this.session = kieContainer.newKieSession("testSession");
-        this.session.setGlobal("armaments", this.armaments);
-        this.session.setGlobal("map", map);
-        this.session.insert(roundCounter);
+        session = kieContainer.newKieSession("testSession");
+        session.setGlobal("armaments", armaments);
+        session.setGlobal("map", roundInput.getMap());
+        session.insert(roundCounter);
 
         for (Player p : roundInput.getPlayers())
-            this.session.insert(p);
+            session.insert(p);
 
         Round round = new Round(roundInput, roundCounter);
-        this.session.insert(round);
+        session.insert(round);
 
         String id;
         PlayerStatus ps;
@@ -49,21 +46,20 @@ public class RecommenderService {
             id = UUID.randomUUID().toString();
             ps.setId(id);
 
-            this.session.insert(ps);
+            session.insert(ps);
 
             for (Armament a : roundInput.getArmaments().get(player)) {
                 a.setPlayerStatus(id);
-                this.session.insert(a);
+                session.insert(a);
             }
         }
     }
 
     public void recommendForRound(RoundInput roundInput) {
-        this.prepareSession(roundInput);
+        prepareSession(roundInput);
 
-        this.session.fireAllRules();
-        this.session.dispose();
-        this.roundCounter++;
-
+        session.fireAllRules();
+        session.dispose();
+        roundCounter++;
     }
 }
